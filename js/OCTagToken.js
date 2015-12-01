f9972776cc5a06737880c6dfb9b8c4770013afd0
Evan Anderson <ejdanderson@gmail.com>
2015-12-01 10:50:13 -0700

    Order by tag type THEN relevance

--- js/OCTagToken.js
+++ js/OCTagToken.js
@@ -16,24 +16,24 @@ oc.TagToken = CFTextToken.extend({
 			return 'oc_tagToken suggestedInline' + (className.length ? ' ' + className : '');
 		}
 	},
-	
+
 	getOverlayClass: function() {
 		if (this.tag.isUserGenerated()) {
-			return 'oc_tagToken userOverlay';			
+			return 'oc_tagToken userOverlay';
 		}
 		else {
 			var className = this.tag.source.getTagTypeClassName();
 			return 'oc_tagToken suggestedOverlay' + (className.length ? ' ' + className : '');
 		}
 	},
-	
+
 	getOverlayFrame: function(inlineFrame, overlayFrame) {
 		var marginDeltaY = parseInt(this.jqInline.css('margin-top'));
 		var marginDeltaX = parseInt(this.jqInline.css('margin-left'));
 		var controlsWidth = (this.tag.bucketName == 'current') ? 70 : 0;
 		var width = controlsWidth + inlineFrame.width;
 		var left = inlineFrame.left - marginDeltaX;
-		
+
 		if (this.tag.bucketName != 'current') {
 			var w = jQuery('span.token-text', this.jqInline).width() - 70;
 			if (this.textWidth > w) {
@@ -49,17 +49,17 @@ oc.TagToken = CFTextToken.extend({
 			height: inlineFrame.height
 		};
 	},
-		
+
 	getContentHTML: function(mode) {
 		if (this.tag.bucketName == 'current' && mode == 'inline') {
 			return '<span class="token-text">' + this.text + '</span>';
 		}
-		
+
 		var addHover = '';
 		if (this.tag.bucketName != 'current' && mode == 'overlay') {
 			addHover = ' hover';
 		}
-		
+
 		var addButton = '<span class="right_textTokenButton' + addHover + ' add" id="add_textTokenButton_' + this.id_tag + '_' + mode + '"></span>';
 		if (this.tag.bucketName == 'current') {
 			addButton = '';
@@ -69,7 +69,7 @@ oc.TagToken = CFTextToken.extend({
 			killButton = '';
 		}
 		var imageButton = '<span class="right_textTokenButton image" id="image_textTokenButton_' + this.id_tag + '_' + mode + '"></span>';
-		
+
 		var title = this.tag.source ? this.text + ' | Relevance: ' + this.tag.source.getNormalizedRelevance().toString().substr(0, 4) : this.text;
 		var text = this.text;
 		var limit = jQuery.browser.msie ? 25 : 28;
@@ -81,7 +81,7 @@ oc.TagToken = CFTextToken.extend({
 		}
 		return killButton + imageButton + addButton + '<span title="' + title + '" id="oc_tagName_' + this.id_tag + '" class="token-text">' + text + '</span>';
 	},
-	
+
 	wasInsertedIntoDOM: function() {
 		this._super();
 		var poppet = this;
@@ -111,7 +111,7 @@ oc.TagToken = CFTextToken.extend({
 				return false;
 			}
 		);
-		
+
 		jQuery('#kill_textTokenButton_' + this.id_tag + '_overlay').hover(
 			function(e) {
 				if (poppet.tag.bucketName == 'suggested') {
@@ -121,7 +121,7 @@ oc.TagToken = CFTextToken.extend({
 					jQuery(this).addClass('hover');
 				}
 			},
-			function(e) { 
+			function(e) {
 				if (poppet.tag.bucketName == 'suggested') {
 					jQuery('#add_textTokenButton_' + poppet.id_tag + '_overlay').addClass('hover');
 				}
@@ -137,24 +137,24 @@ oc.TagToken = CFTextToken.extend({
 				}
 				jQuery(this).addClass('hover');
 			},
-			function(e) { 
+			function(e) {
 				if (poppet.tag.bucketName != 'current') {
 					jQuery('#add_textTokenButton_' + poppet.id_tag + '_overlay').addClass('hover');
 				}
 				jQuery(this).removeClass('hover');
 			}
 		);
-		
+
 		// figure out default width of our text
 		this.textWidth = jQuery('<span class="dummy_token-text" id="dummy_' + this.id_tag + '">' + this.text + '</span>').appendTo('body').width();
 		jQuery('#dummy_' + this.id_tag).remove();
-		
+
 		cf.disableTextSelection(this.jqInline.get(0));
 		cf.disableTextSelection(jQuery('span.token-text', this.jqInline).get(0));
 		cf.disableTextSelection(this.jqOverlay.get(0));
 		cf.disableTextSelection(jQuery('span.token-text', this.jqOverlay).get(0));
 	},
-	
+
 	wasRemovedFromDOM: function() {
 		this.hideTooltip();
 	},
@@ -167,7 +167,7 @@ oc.TagToken = CFTextToken.extend({
 	},
 
 	showTooltip: function() {
-		
+
 		// in case we missed the hover-off
 		this.hideTooltip();
 		this.jqTooltip = jQuery('\
@@ -178,7 +178,7 @@ oc.TagToken = CFTextToken.extend({
 
 		var overlayFrame = cf.pageFrame(this.jqOverlay.get(0));
 		var tooltipFrame = cf.pageFrame(this.jqTooltip.get(0));
-				
+
 		var left = overlayFrame.left + 25;
 		var w = Math.max(
 				tooltipFrame.width - parseInt(this.jqTooltip.css('padding-left')) - parseInt(this.jqTooltip.css('padding-right')),
@@ -194,26 +194,27 @@ oc.TagToken = CFTextToken.extend({
 			visibility:'visible',
 			display:'none'
 		};
-		this.jqTooltip.css(css).show();		
+		this.jqTooltip.css(css).show();
 	},
-	
+
 	hideTooltip: function() {
 		if (this.jqTooltip) {
 			var poppet = this;
 			this.jqTooltip.hide('fast', function() { poppet.jqTooltip.remove(); } );
-		}		
+		}
 	},
-	
+
 	getSortKey: function() {
 		if (this.tag) {
 			if (this.tag.isUserGenerated()) {
 				return Number.MAX_VALUE;
 			}
-			return parseFloat(this.tag.source.getNormalizedRelevance());
+			// Order by tag type then relevance
+			return this.tag.type + "" + parseFloat(this.tag.source.getNormalizedRelevance());
 		}
 		return this._super();
 	},
-		
+
 	tag: null,	// NB this is a ref to the full-fledged tag object, not just a string
 	jqTooltip: null,
 	textWidth: -1
